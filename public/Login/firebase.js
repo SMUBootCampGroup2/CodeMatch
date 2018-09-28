62e5a4d36ddc112e932241bc39fbb64d5c1a5cac
peterj330 <peter.a.jarrard@gmail.com>
2018-09-28 00:44:20 -0500

    asdf

---
+++ public/Login/firebase.js
@@ -0,0 +1,13 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDmy45P5faEmQB7A6fxpTPYqia4aWM4z6c",
+    authDomain: "codematch-grp2.firebaseapp.com",
+    databaseURL: "https://codematch-grp2.firebaseio.com",
+    projectId: "codematch-grp2",
+    storageBucket: "codematch-grp2.appspot.com",
+    messagingSenderId: "451759923507"
+  };
+  firebase.initializeApp(config);
+  var db = firebase.database();
+
