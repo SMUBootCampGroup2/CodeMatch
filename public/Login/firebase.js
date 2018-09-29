1fd4de11af21e32d1674566fd34ffdfbef9a9d5a
peterj330 <peter.a.jarrard@gmail.com>
2018-09-29 13:27:04 -0500

    asdf

--- public/Login/firebase.js
+++ public/Login/firebase.js
@@ -12,41 +12,39 @@ firebase.initializeApp(config);
 var db = firebase.database();
 var username ;
 
-// =====================================================================
+//=====================================================================
 
-// $(document).ready(function () {
-//   //	Listens for users signing in or out
-//   firebase.auth().onAuthStateChanged(function (user) {
-//     if (user && username != '') {
-//       //	User successfully created
-//       user.updateProfile({
-//         displayName: username
-//       }).then(function () {
-//         db.ref('users/' + user.uid + '/userInfo').set({
-//           displayName: user.displayName,
-//           email: user.email
-//         });
+$(document).ready(function () {
+  //	Listens for users signing in or out
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user && username != '') {
+      //	User successfully created
+      user.updateProfile({
+        displayName: username
+      }).then(function () {
+        db.ref('users/' + user.uid + '/userInfo').set({
+          displayName: user.displayName,
+          email: user.email
+        });
 
-//         sendEmailVerification();
-//         window.location.href = '../index.html';
-//       }, function (error) {
-//         alert(error.message);
-//       });
-//     }
-//   });
-//   // =====================================================================
-
-//   $('#signup-btn').click(function (event) {
-//     event.preventDefault();
-//     signUp();
-//   });
-//   //	Toggles Help Info on hover
-//   $('.help-icon').hover(function () {
-//     $('.tooltip-text').removeClass('js-hidden');
-//   }, function () {
-//     $('.tooltip-text').addClass('js-hidden');
-//   });
-// });
+        sendEmailVerification();
+        window.location.href = '../index.html';
+      }, function (error) {
+        alert(error.message);
+      });
+    }
+  });
+  $('#signup-btn').click(function (event) {
+    event.preventDefault();
+    signUp();
+  });
+  //	Toggles Help Info on hover
+  $('.help-icon').hover(function () {
+    $('.tooltip-text').removeClass('js-hidden');
+  }, function () {
+    $('.tooltip-text').addClass('js-hidden');
+  });
+});
 
 $(document).ready(function () {
   firebase.auth().onAuthStateChanged(function (user) {
